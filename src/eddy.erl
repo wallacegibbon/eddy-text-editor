9177dfcac10fc8204496845fe68294851d50a0c4
wallace <wallacegibbon@outlook.com>
2020-03-14 23:48:59 +0800

    .

--- src/eddy.erl
+++ src/eddy.erl
@@ -5,26 +5,16 @@
 -include_lib("cecho/include/cecho.hrl").
 -define(SPACEKEY, 32).
 
-start() ->
-    wordsvc:start_link(),
-    application:start(cecho),
-    ok = cecho:noecho(),
-    %cecho:move(1, 1),
-    {Row, Col} = cecho:getyx(),
-    {MRow, MCol} = cecho:getmaxyx(),
-    read_chars(),
-    %application:stop(cecho),
-    ok.
+translate_charlst([C | Rst]) -> case translate_char(C) of
+				    {ok, V} -> [V | translate_charlst(Rst)];
+				    _ -> translate_charlst(Rst)
+				end;
+translate_charlst([]) -> [].
 
-read_chars() ->
-    case cecho:getch() of 
-	?SPACEKEY ->
-	    cecho:addch(?SPACEKEY),
-	    cecho:refresh();
-	C ->
-	    try_getword([C])
-    end,
-    read_chars().
+translate_char(C) ->
+    KeyMap = #{$w => 1, $e => 2, $r => 3, $s => 4, $d => 5, $f => 6,
+	       $x => 7, $c => 8, $v => 9},
+    maps:find(C, KeyMap).
 
 try_getword(CList) ->
     case cecho:getch() of
@@ -40,18 +30,25 @@ try_getword(CList) ->
 	    try_getword([C | CList])
     end.
 
-translate_charlst([C | Rst]) ->
-    case translate_char(C) of
-	{ok, V} ->
-	    [V | translate_charlst(Rst)];
-	_ ->
-	    translate_charlst(Rst)
-    end;
-translate_charlst([]) ->
-    [].
+read_chars() ->
+    handle(cecho:getch()),
+    read_chars().
 
-translate_char(C) ->
-    KeyMap = #{$w => 1, $e => 2, $r => 3, $s => 4, $d => 5, $f => 6,
-	       $x => 7, $c => 8, $v => 9},
-    maps:find(C, KeyMap).
+handle(?SPACEKEY) ->
+    cecho:addch(?SPACEKEY),
+    cecho:refresh();
+handle(C) ->
+    try_getword([C]).
+
+
+start() ->
+    wordsvc:start_link(),
+    application:start(cecho),
+    ok = cecho:noecho(),
+    %cecho:move(1, 1),
+    %{Row, Col} = cecho:getyx(),
+    %{MRow, MCol} = cecho:getmaxyx(),
+    read_chars(),
+    %application:stop(cecho),
+    ok.
 
