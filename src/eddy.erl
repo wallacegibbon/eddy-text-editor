e1e78e40148ffa9fa3627b342c2335e4db8a184d
wallace <wallacegibbon@aliyun.com>
2024-08-05 15:58:30 +0800

    Trivial code style changes: Using hard `TAB`s instead of spaces. (In Erlang we won't need so many indentations, using hard TAB is a good choice here)

--- src/eddy.erl
+++ src/eddy.erl
@@ -1,14 +1,14 @@
 -module(eddy).
-
 -export([start/0, main/1]).
 
 -spec start() -> no_return().
 start() ->
-    {ok, _} = eddy_t9_translator:start_link(),
-    {ok, _} = eddy_state_machine:start_link(),
-    {ok, _} = eddy_edit_event:start(),
-    ok = eddy_edit_event:add_handler(eddy_terminal_ui),
-    ok.
+	{ok, _} = eddy_t9_translator:start_link(),
+	{ok, _} = eddy_state_machine:start_link(),
+	{ok, _} = eddy_edit_event:start(),
+	ok = eddy_edit_event:add_handler(eddy_terminal_ui),
+	ok.
 
 main([]) ->
-    start().
+	start().
+
